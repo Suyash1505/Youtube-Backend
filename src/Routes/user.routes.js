4e777acefd206d37f5920700269ce0e5b9a7951f
Suyash1505 <adv.mks7171@gmail.com>
2025-08-03 10:21:36 +0530

    add user router

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../Controllers/user.controllers.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../Controllers/user.controllers.js";
 import { upload } from "../Middlewears/multer.middlewears.js";
 import { verifyJWT } from "../Middlewears/auth.middlewears.js";
 
@@ -23,5 +23,11 @@ router.route("/login").post(loginUser);
 
 // Secured Route
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router.route("/avatar").patch(verifyJWT, upload.file("avatar"), updateUserAvatar);
+router.route("/cover-Image").patch(verifyJWT, upload.file("coverImage"), updateUserCoverImage);
 export default router;
